b96a3bd0f694a2b817cdbcf700e32e6f84f1d14e
nanborklabs <nandhu12195@gmail.com>
2017-09-02 16:35:38 +0530

    QR Save,Scan Dynamic

--- app/src/main/java/com/versalite/assettracking/Activity/AssetAddActivity.java
+++ app/src/main/java/com/versalite/assettracking/Activity/AssetAddActivity.java
@@ -5,47 +5,69 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.versalite.assettracking.AppRequestQueue;
 import com.versalite.assettracking.R;
 
-import java.util.List;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
  * Created by nandhu on 24/8/17.
- *
  */
 
-public class AssetAddActivity extends AppCompatActivity {
+public class AssetAddActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, AdapterView.OnItemSelectedListener, Response.ErrorListener, Response.Listener<JSONObject> {
 
 
+    private static final String ADD_ASSETS = ""; //// TODO: 2/9/17  Add API
     String[] items = new String[]{"Capital", "Non-Capital", "Add New Cateogry"};
     String[] descss = new String[]{"Table", "Chair", "Desks", "Add New Description"};
-    @BindView(R.id.textInputLayout)
-    TextInputLayout textInputLayout;
+
+
+    String saveAssetURL = "https://us-central1-audit-app-819d8.cloudfunctions.net/app/saveAsset";
+
+    String encodingURL  = "https://us-central1-audit-app-819d8.cloudfunctions.net/app/getAssetDetails";
+
+    String Name;
+    String AssetId;
+    String assetType;
+    String assetDescription;
+    String AsssetTag;
+    String orgname;
+    String  location;
+    String deptName;
+    String roomNo;
     @BindView(R.id.asset_name)
-    TextView assetName;
+    TextInputLayout mAssetName;
     @BindView(R.id.asset_type_spinner)
-    Spinner assetTypeSpinner;
+    Spinner mSpinner;
     @BindView(R.id.asset_desc)
-    TextView assetDesc;
-    @BindView(R.id.asset_desc_spinner)
-    Spinner assetDescSpinner;
-    @BindView(R.id.asset_tag)
-    TextView assetTag;
-    @BindView(R.id.ll)
-    LinearLayout ll;
+    EditText mAssetDesc;
+    @BindView(R.id.o_ed)
+    EditText oEd;
+    @BindView(R.id.l_ed)
+    EditText lEd;
+    @BindView(R.id.d_ed)
+    EditText dEd;
+    @BindView(R.id.r_ed)
+    EditText rEd;
     @BindView(R.id.save_button)
-    Button addButton;
+    Button saveButton;
+    private String TAG ="Spinner";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -55,31 +77,105 @@ public class AssetAddActivity extends AppCompatActivity {
 
 
         // CApital Spiiner
-        Spinner spinner = (Spinner) findViewById(R.id.asset_type_spinner);
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        mSpinner.setAdapter(adapter);
+        mSpinner.setOnItemClickListener(this);
+        mSpinner.setOnItemSelectedListener(this);
 
         // Description Spinner
-        Spinner desc = (Spinner) findViewById(R.id.asset_desc_spinner);
-        ArrayAdapter<String> adaptera = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, descss);
-        adaptera.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        desc.setAdapter(adaptera);
-        addButton.setOnClickListener(new View.OnClickListener() {
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 addToAssets();
             }
-
-
         });
+    }
+
+    private void addToAssets() {
+        Name = mAssetName.getEditText().getText().toString();
+        orgname = oEd.getText().toString();
+        location = lEd.getText().toString();
+        deptName = dEd.getText().toString();
+        roomNo = rEd.getText().toString();
+        assetDescription = mAssetDesc.getText().toString();
+        // We have all the Variables needed to add assets
+        saveAsset(Name,orgname,location,deptName,roomNo);
+    }
+
+    private void saveAsset(String name, String orgname, String location, String deptName, String roomNo) {
+        JSONObject mJsonObject = new JSONObject();
+        try {
+
+
+            mJsonObject.put("assetName",name);
+            mJsonObject.put("orgName",orgname);
+            mJsonObject.put("location",location);
+            mJsonObject.put("deptName",deptName);
+            mJsonObject.put("type",assetType);
+            mJsonObject.put("assetType",assetDescription);
+            mJsonObject.put("roomName",roomNo);
+            JsonObjectRequest mRequest = new JsonObjectRequest(1,saveAssetURL,mJsonObject,this,this);
+            AppRequestQueue mAppRequestQueue = AppRequestQueue.getInstance(this);
+            mAppRequestQueue.addToRequestQue(mRequest);
+
+        }
+        catch (Exception e){
+            Log.d(TAG, "saveAsset: Exceotion");
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
     }
-    private void addToAssets() {
-        Toast.makeText(getApplicationContext(),"Added !..",Toast.LENGTH_LONG).show();
-        // Goto new Activity Here
-        Intent i = new Intent(this,QRViewActivity.class);
-        startActivity(i);
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Log.d(TAG, "onItemSelected: " + items[position]);
+        assetType = items[position];
     }
-    
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Toast.makeText(getApplicationContext(),"Some Error Occured ",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        Log.d(TAG, "onResponse: "+response.toString());
+        try {
+            if (response.getInt("status") == 1 ){
+
+                long id = response.getLong("id");
+                String QRcode = encodingURL + "?id="+id;
+                Intent i = new Intent(this,QRViewActivity.class);
+                i.putExtra("qr",QRcode);
+                i.putExtra("id",id);
+                i.putExtra("name",response.getString("name"));
+                i.putExtra("orgname",response.getString("orgName"));
+                i.putExtra("location",response.getString("location"));
+                i.putExtra("deptName",response.getString("deptName"));
+                i.putExtra("roomName",response.getString("roomName"));
+                i.putExtra("type",response.getString("type"));
+                i.putExtra("description",response.getString("description"));
+
+                startActivity(i);
+
+
+
+
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- app/src/main/java/com/versalite/assettracking/Activity/QRViewActivity.java
+++ app/src/main/java/com/versalite/assettracking/Activity/QRViewActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.google.zxing.BarcodeFormat;
@@ -34,15 +35,37 @@ public  class QRViewActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.qr_view_acitivity);
+
         ButterKnife.bind(this);
+
+        if (getIntent() == null){
+            throw new NullPointerException("Intent cannot be null");
+        }
+        String qrToCode = getIntent().getStringExtra("qr");
+        String Name = getIntent().getStringExtra("name");
+        String AssetId  = getIntent().getStringExtra("id");
+        String assetType= getIntent().getStringExtra("type");
+        String assetDescription = getIntent().getStringExtra("description");
+//        String AsssetTag = getIntent().getStringExtra();
+        String orgname = getIntent().getStringExtra("orgname");
+        String  location = getIntent().getStringExtra("location");
+        String deptName = getIntent().getStringExtra("deptName");
+        String roomNo = getIntent().getStringExtra();
+
+
         ImageView imageView = (ImageView) findViewById(R.id.qr_view);
         try {
-            Bitmap bitmap = encodeAsBitmap("Fuck_the_World.com");
+            Bitmap bitmap = encodeAsBitmap(qrToCode);
             imageView.setImageBitmap(bitmap);
         } catch (WriterException e) {
             e.printStackTrace();
 
         }
+
+
+//        view.setDrawingCacheEnabled(true);
+//        Bitmap b = view.getDrawingCache();
+//        b.compress(CompressFormat.JPEG, 95, new FileOutputStream("/some/location/image.jpg"));
     }
 
 
